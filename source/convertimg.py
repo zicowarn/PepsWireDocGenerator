c5dcb297e292e02e2544ff298a554e278ccef60a
Barbossa <zicowarn@hotmail.com>
2024-01-18 17:08:18 +0100

    更新convertimg.py文件
    更新htm中img的高度设置，将其宽度设置为auto

--- source/convertimg.py
+++ source/convertimg.py
@@ -2,6 +2,13 @@ import os
 import re
 from PIL import Image # pip install Pillow
 
+'''
+Created on 17.01.2024
+Updated on 18.01.2024
+
+@author: wang
+'''
+
 try:
     from myapp import settings  # @UnusedImport
 except:
@@ -68,6 +75,44 @@ def convert_git2jpg_update_h2t(static_base_path, h2t_record_path):
     if os.path.exists(h2t_record_path):
         with open(h2t_record_path, 'w', encoding='utf-8') as fw:
             fw.write(h2t_content)
+            
+            
+def update_templates_imgsize(templates_base_path):
+    """change img node attribute height is auto, width is auto as default
+
+    Args:
+        static_base_path (_type_): _description_
+        templates_base_path (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    list_templates = []
+    for root, dirs, files in os.walk(templates_base_path, topdown=False):
+        if len(files) != 0:
+            # iterate the files list
+            for single_file in list(sorted(files)):
+                # convert only htm files
+                if not single_file.endswith('.htm'):
+                    continue
+                # set target path
+                source_file = os.path.join(root, single_file)
+                list_templates.append(source_file)
+    if (list_templates) == 0:
+        return False
+    for file_templae in list_templates:
+        print(f"# 正在处理模板: {file_templae}")
+        template_content = ""
+        is_updated = False
+        with open(file_templae, 'r', encoding='utf-8') as fr:
+            template_content = fr.read()
+        patternwh = r'(<img[^>]*\s)width="\d+"'
+        matchwh = re.search(patternwh, template_content)
+        if matchwh :
+            if matchwh:
+                template_content = re.sub(patternwh, r'\1width="auto"', template_content)
+            with open(file_templae, 'w', encoding='utf-8') as fw:
+                fw.write(template_content)
 
 def update_templates_imgsrc(static_base_path, templates_base_path):
     list_jpgimgaes = []
@@ -160,7 +205,8 @@ def main():
     h2t_record_path = os.path.join(settings.BASE_DIR, 'htmlataghref.h2t')
     templates_base_path = os.path.join(settings.BASE_DIR, 'templates')
     # convert_git2jpg_update_h2t(static_base_path, h2t_record_path)
-    update_templates_imgsrc(static_base_path, templates_base_path)
+    # update_templates_imgsrc(static_base_path, templates_base_path)
+    update_templates_imgsize(templates_base_path)
     
 if __name__ == '__main__':
     main()
